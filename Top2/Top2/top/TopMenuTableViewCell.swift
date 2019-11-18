e309624eea3ceb5bd02324a9405e06ce034ae22d
Noriaki Sakai <avengers3000ily@gmail.com>
2019-11-19 00:09:57 +0700

    ADD tableview

---
+++ Top2/Top2/top/TopMenuTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TopMenuTableViewCell.swift
+//  Top2
+//
+//  Created by 酒井典昭 on 2019/11/13.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+
+class TopMenuTableViewCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
